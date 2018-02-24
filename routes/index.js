7099c9a0e9fc694b91b95aefc014fd8984f3a4ab
gerarddo <germijares280995@hotmail.com>
2018-02-23 19:31:20 -0600

    added interesting link

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,6 @@ router.get("/", function(req, res){
     res.render("index")
 }); //router.get
 
-router.get("/tribute", function(req, res){
-    res.render("01-Tribute")
-}); //router.get
-
-router.get("/portfolio", function(req, res){
-    res.render("02-Portfolio")
-}); //router.get
-
-router.get("/quote", function(req, res){
-    res.render("03-Quote")
-}); //router.get
-
-router.get("/weather", function(req, res){
-    res.render("04-weather")
-}); //router.get
-
 router.get("*", function(req, res){
     res.redirect("/")
 }); //router.get
