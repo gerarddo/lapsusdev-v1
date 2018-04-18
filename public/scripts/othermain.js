ea40b0daceca1e9ed897136c1b70700ec3edd2b0
gerarddo <germijares280995@hotmail.com>
2018-04-18 13:26:42 -0500

    done for now

--- public/scripts/othermain.js
+++ public/scripts/othermain.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
   function setWindowHeightTo(htmlClass) {
-    windowHeight = $(window).innerHeight();
-    $(htmlClass).css('height', windowHeight);
+
+    let _welcomeHeight = $("#welcome").innerHeight();
+    console.log(_welcomeHeight)
+    let windowHeight = $(window).innerHeight() - _welcomeHeight;
+    $(htmlClass).css('height', windowHeight );
   };
 
-  function setVerticalCenterPositionTo(htmlClass) {
-    let windowHeight = $(window).innerHeight();
-    let elHeight = $(htmlClass).height();
-    let marginTop = windowHeight/2 - elHeight/2;
+  // function setVerticalCenterPositionTo(htmlClass) {
+  //   let windowHeight = $(window).innerHeight();
+  //   let elHeight = $(htmlClass).height();
+  //   let marginTop = windowHeight/2 - elHeight/2;
 
-    $(htmlClass).css('margin-top', marginTop);
-  };
+  //   $(htmlClass).css('margin-top', marginTop);
+  // };
 
-  setWindowHeightTo('.windowHeight');
-  setVerticalCenterPositionTo('.yCenterPosition');
+  setWindowHeightTo('.fillRemainingHeight');
+  // setVerticalCenterPositionTo('.yCenterPosition');
   
   $(window).resize(function() {
-    setWindowHeightTo('.windowHeight');
-    setVerticalCenterPositionTo('.yCenterPosition');
+    setWindowHeightTo('.fillRemainingHeight');
+    // setVerticalCenterPositionTo('.yCenterPosition');
   });
 });
+
