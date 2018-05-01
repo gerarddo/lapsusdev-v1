8269c6b75bfb5a7e530a18f6681b92719c456ca5
gerarddo <germijares280995@hotmail.com>
2018-04-30 20:42:46 -0500

    did a little advance with the about section

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get("/", function(req, res){
 router.get("/portfolio", function(req, res){
     res.render("portfolio", {route: "/portfolio", projects: info.projects})
 }); //router.get
+router.get("/about", function(req, res){
+    res.render("about", {route: "/about", about: info.about})
+}); //router.get
 router.get("/old", function(req, res){
     res.render("old")
 }); //router.get
